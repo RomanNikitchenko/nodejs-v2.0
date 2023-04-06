1f1f47095710018548a526cc990194a3be033bed
RomanNikitchenko <nikitchenkoroman25@gmail.com>
2023-04-06 13:24:40 +0300

    listContacts

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,17 +1,18 @@
 const express = require("express");
-const { v4 } = require('uuid');
-const contacts = require("../../models/contacts.json");
+const { v4 } = require("uuid");
+const contactsOperations = require("../../models/contacts");
 
 const router = express.Router();
 
 //get
 router.get("/", async (req, res, next) => {
+  const contacts = await contactsOperations.listContacts();
   res.json({
     status: "success",
     code: 200,
     data: {
-      result: contacts
-    }
+      result: contacts,
+    },
   });
 });
 
@@ -19,34 +20,34 @@ router.get("/", async (req, res, next) => {
 router.get("/:contactId", async (req, res, next) => {
   const { contactId } = req.params;
   console.log(req.params);
-  const result = contacts.find(item => item.id === contactId);
+  const result = contacts.find((item) => item.id === contactId);
   if (!result) {
     res.status(404).json({
       status: "error",
       code: 404,
-      message: `Produst with id=${contactId} not found`
-    })
+      message: `Produst with id=${contactId} not found`,
+    });
   }
   res.json({
     staus: "success",
     code: 200,
     data: {
       result: result,
-    }
+    },
   });
 });
 
 //post
 router.post("/", async (req, res, next) => {
   const newProduct = { ...req.body, id: v4() };
-  contacts.push(newProduct)
+  contacts.push(newProduct);
   res.status(201).json({
-    status: 'success',
+    status: "success",
     code: 201,
     data: {
-      result: newProduct
-    }
-  })
+      result: newProduct,
+    },
+  });
 });
 
 //delete
